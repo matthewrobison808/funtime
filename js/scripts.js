92877410f613e1e536065e449746672043cdfc9e
matthewrobison818 <matthewrobison818@gmail.com>
2017-02-02 14:21:41 -0800

    Wite js code to scramble sentence

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  var saySomething = prompt("Do you have anything to say?");
+
+  var first = saySomething.charAt(0);
+  var last = saySomething.charAt(saySomething.length-1);
+
+  var firstCapitalized = first.toUpperCase();
+  var lastCapitalized = last.toUpperCase();
+
+  var firstResponse = (firstCapitalized + lastCapitalized);
+  var lastResponse = (lastCapitalized + firstCapitalized);
+
+  var charLength = saySomething.length;
+
+  var newBeginning = charLength / 2;
+
+  var newsentence = alert("" + saySomething.charAt(newBeginning) + saySomething + firstResponse);
+
+});
